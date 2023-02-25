10f4a5599477e6f016c240482964fbca1f775596
108028 <125982792+108028@users.noreply.github.com>
2023-02-25 19:53:05 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.pause(100)
+basic.showLeds(`
+    . . # . .
+    # . # . #
+    # . # . #
+    # . . . #
+    . # # # .
+    `)
+basic.pause(1000)
+basic.clearScreen()
+basic.forever(function () {
+	
+})
